c46e39cc68f693fb2ca39429d34ccd2264b589f4
ramy elbanna <ramyalbanna12@gmail.com>
2025-03-01 13:23:26 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Product> ProductsList = [
+
+            new()
+            {
+                    Id = 1,
+                    Name = "Product 1"
+            },
+            new()
+            {
+                    Id = 2,
+                    Name = "Product 2"
+            },
+        ];
+
+        ShowProducts(ProductsList);
+    }
+
+    static void ShowProducts(List<Product> ProductsList)
+    {
+        foreach (Product product in ProductsList)
+        {
+            Console.WriteLine(product.Name);
+        }
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
